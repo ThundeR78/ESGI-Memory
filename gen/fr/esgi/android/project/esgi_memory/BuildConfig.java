bcf2580eac4a56f4a7465ee16412e40582acfa13
ThundeR78 <julienwetstein@gmail.com>
2013-11-17 00:44:27 +0100

    commit initial du projet

---
+++ gen/fr/esgi/android/project/esgi_memory/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package fr.esgi.android.project.esgi_memory;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
